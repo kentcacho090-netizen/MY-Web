c8e3e93c6b54521218eb2393ff31cb63ed5525de
kentcacho090-netizen <kentcacho090@gmail.com>
2026-08-29 09:12:59 +0800

    Add files via upload

---
+++ App.js.js
@@ -0,0 +1,11 @@
+import QuizPlatform from './QuizPlatform';
+
+function App() {
+  return (
+    <div style={{ minHeight: '100vh', backgroundColor: '#fff' }}>
+      <QuizPlatform />
+    </div>
+  );
+}
+
+export default App;
